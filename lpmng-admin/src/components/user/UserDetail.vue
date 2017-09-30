2344e2a12881388cd8b252c7616f2c5a8892ea52
Tjiho Lun <tom.darboux@eisti.fr>
2017-10-01 00:44:04 +0200

    get users rely to core

--- lpmng-admin/src/components/user/UserDetail.vue
+++ lpmng-admin/src/components/user/UserDetail.vue
@@ -2,18 +2,21 @@
   <main class=""> 
     <sub-menu-vertical></sub-menu-vertical>
     <nav id="searchmenu">
-        
+        <div class="infos negatif" v-if="msgError">{{msgError}}</div>
         <input type="search" v-model="search">
         <!-- results search (use radio input to permit the utilisation of v-bind properties)-->
         <div v-for="user in pseudos">
-          <input type="radio" name="search" class="search-result" v-bind:value="user.pseudo" v-bind:id="'id-'+user.pseudo" v-model="pseudoSelected"> 
-          <label v-bind:for="'id-'+user.pseudo">
-              {{user.pseudo}}
+          <input type="radio" name="search" class="search-result" v-bind:value="user.uid" v-bind:id="'id-'+user.uid" v-model="pseudoSelected"> 
+          <label v-bind:for="'id-'+user.uid">
+              
+              <div>{{user.uid}}</div>
+              <small>{{user.commonname}} {{user.surname}}</small>
+              <div class="separateur"></div>
           </label>        
         </div>
 
     </nav>
-    <infos-user v-if="pseudoSelected" v-bind:pseudo="pseudoSelected" v-bind:nom="listUsers[pseudoSelected].name" v-bind:prenom="listUsers[pseudoSelected].firstname" v-bind:cotisant="listUsers[pseudoSelected].cotisant" v-bind:nombreSessions="listUsers[pseudoSelected].nombreSessions">
+    <infos-user v-if="pseudoSelected" v-bind:pseudo="pseudoSelected" v-bind:nom="listUsers[pseudoSelected].surname" v-bind:prenom="listUsers[pseudoSelected].commonname" v-bind:cotisant="listUsers[pseudoSelected].cotisant" v-bind:nombreSessions="listUsers[pseudoSelected].nombreSessions">
 
     </infos-user>
 
@@ -31,7 +34,8 @@ export default {
     return {
       search: '',
       pseudoSelected: '',
-      listUsers: {}
+      listUsers: {},
+      msgError: ''
     }
   },
   watch:
@@ -48,24 +52,32 @@ export default {
         console.log('-----search:' + this.search + '--------')
         if (this.search.length > 0) {
           for (var key in this.listUsers) {
-            // console.log(element.pseudo)
-            // /console.log(element.pseudo.indexOf(this.search))
-            if (this.listUsers[key].pseudo.indexOf(this.search) > -1 || this.listUsers[key].firstname.indexOf(this.search) > -1 || this.listUsers[key].name.indexOf(this.search) > -1) {
+            console.log(this.listUsers[key].uid)
+            console.log(this.listUsers[key].uid.indexOf(this.search))
+            if (this.listUsers[key].uid.indexOf(this.search) > -1 || this.listUsers[key].commonname.indexOf(this.search) > -1 || this.listUsers[key].surname.indexOf(this.search) > -1) {
               pseudosTmp.push(this.listUsers[key])
             }
           }
         }
+        console.log(pseudosTmp)
         return pseudosTmp
       }
     }
   },
   mounted: function () {
     axios.get('http://127.0.0.1:8000/users/', {})
-    .then(function (response) {
+    .then((response) => {
       console.log(response)
+
+      response.data.forEach(function (element) {
+        this.listUsers[element.uid] = element
+        this.listUsers[element.uid].cotisant = 'non'
+        this.listUsers[element.uid].nombreSessions = '0'
+      }, this)
     })
-    .catch(function (error) {
+    .catch((error) => {
       console.log(error)
+      this.msgError = 'Erreur de la requète'
     })
   },
   components:

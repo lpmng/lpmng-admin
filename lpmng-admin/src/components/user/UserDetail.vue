581e0704bddd4f273c3786339fff0a8dd65f50cc
Gaël Berthaud-Müller <blacksponge@tuta.io>
2017-10-11 17:23:57 +0200

    users modification

--- lpmng-admin/src/components/user/UserDetail.vue
+++ lpmng-admin/src/components/user/UserDetail.vue
@@ -1,38 +1,27 @@
 <template>
-  <main class=""> 
+  <main class="">
     <sub-menu-vertical></sub-menu-vertical>
     <section id="content" class="searchUser">
       <h1>Rechercher un utilisateur</h1>
       <div class="infos negatif" v-if="msgError">{{msgError}}</div>
       <input type="search" v-model="search">
-    
+
     <!-- results search (use radio input to permit the utilisation of v-bind properties)-->
       <div v-for="user in pseudos" class="searchResult">
-        <input type="radio" name="search" class="search-result" v-bind:value="user.uid" v-bind:id="'id-'+user.uid" v-model="pseudoSelected"> 
+        <input type="radio" name="search" class="search-result" v-bind:value="user.uid" v-bind:id="'id-'+user.uid" v-model="pseudoSelected">
         <label v-bind:for="'id-'+user.uid">
-            
+
             <div class="username">{{user.uid}}</div>
             <div class="name">{{user.commonname}} {{user.surname}}</div>
             <div class="actions">
               <img src="../../../src/assets/IcoMoon/SVG/273-checkmark.svg" class="button"/>
-              <img src="../../../src/assets/IcoMoon/SVG/006-pencil.svg" class="button" @click="displayChangeUser"/>
+              <img src="../../../src/assets/IcoMoon/SVG/006-pencil.svg" class="button" @click="displayChangeUser(user)"/>
               <img src="../../../src/assets/IcoMoon/SVG/173-bin.svg" class="button" @click="displaySupprUser" v-bind:pseudo="user.uid"/>
             </div>
-        </label>        
+        </label>
       </div>
     </section>
-    <!--
-    <infos-user v-if="pseudoSelected" v-bind:pseudo="pseudoSelected"                        v-bind:nom="listUsers[pseudoSelected].surname" 
-                                      v-bind:prenom="listUsers[pseudoSelected].commonname"  v-bind:cotisant="listUsers[pseudoSelected].cotisant" 
-                                      v-bind:nombreSessions="listUsers[pseudoSelected].nombreSessions"
-                                      v-bind:tel="listUsers[pseudoSelected].tel"            v-bind:mail="listUsers[pseudoSelected].mail">
 
-    </infos-user>
-    -->
-
-    <!--    ================== Modals ===================    !-->
-
-    <!-- delete modal -->
     <div class="modal-back" v-if="supprUserVisible">
       <div class="modal">
         <h2>Supprimer {{pseudoSelected}} ?</h2>
@@ -46,44 +35,40 @@
     <div class="modal-back" v-if="changeUserVisible">
       <div class="modal">
         <h2>Modification de <span class="username">{{pseudoSelected}}</span> </h2>
-        <form>
+        <form v-on:submit="changeUser">
           <!-- nom -->
           <div class="input">
-          <label for="nom">Nom:</label>
-          <input type="text" name="nom" id="nom"/>
-          <a class="button">Modifier</a>
+            <label for="nom">Nom:</label>
+            <input type="text" name="nom" id="nom" v-model="userSelected.surname"/>
           </div>
           <!-- prenom -->
           <div class="input">
-          <label for="prenom">Prénom:</label>
-          <input type="text" name="prenom" id="prenom"/>
-          <a class="button">Modifier</a>
+            <label for="prenom">Prénom :</label>
+            <input type="text" name="prenom" id="prenom" v-model="userSelected.commonname"/>
           </div>
           <!-- mail -->
           <div class="input">
-          <label for="mail">Mail:</label>
-          <input type="mail" name="pseudo" id="mail"/>
-          <a class="button">Modifier</a>
+            <label for="mail">Mail :</label>
+            <input type="mail" name="pseudo" id="mail" v-model="userSelected.mail"/>
           </div>
           <!-- tel -->
           <div class="input">
-          <label for="telephone">Téléphone:</label>
-          <input type="tel" name="telephone" id="telephone"/>
-          <a class="button">Modifier</a>
+            <label for="telephone">Téléphone :</label>
+            <input type="tel" name="telephone" id="telephone" v-model="userSelected.tel"/>
           </div>
 
           <div class="input">
-          <label for="password">Mot de passe:</label>
-          <input type="password" name="password" id="password"/>
-          <a class="button">Modifier</a>
+            <label for="password">Mot de passe :</label>
+            <input type="password" name="password" id="password" v-model="userSelected.password"/>
           </div>
 
+          <input type="button" value="Retour" @click="hideChangeUser">
+          <input type="submit" value="Modifier">
         </form>
-        <a class="button" @click="displayChangeUser">Retour</a>
       </div>
     </div>
   </main>
-  
+
 </template>
 
 <script>
@@ -96,6 +81,7 @@ export default {
     return {
       search: '',
       pseudoSelected: '',
+      userSelected: {},
       listUsers: {},
       msgError: '',
       pseudos: {},
@@ -107,9 +93,7 @@ export default {
   watch:
   {
     search: function (val) {
-      console.log('yo')
       var pseudosTmp = []
-      console.log('-----search:' + this.search + '--------')
       for (var key in this.listUsers) {
         if (this.listUsers[key].uid.indexOf(val) > -1 || this.listUsers[key].commonname.indexOf(val) > -1 || this.listUsers[key].surname.indexOf(val) > -1) {
           pseudosTmp.push(this.listUsers[key])
@@ -122,19 +106,46 @@ export default {
     displaySupprUser () {
       this.supprUserVisible = !this.supprUserVisible
     },
-    displayChangeUser () {
-      this.changeUserVisible = !this.changeUserVisible
+    displayChangeUser (user) {
+      this.changeUserVisible = true
+      this.userSelected = Object.assign({}, user)
+    },
+    hideChangeUser () {
+      this.changeUserVisible = false
     },
     displayMenu () {
-      console.log('wout')
       this.menuVisible = !this.menuVisible
+    },
+    changeUser (e) {
+      e.preventDefault()
+
+      let finalObj = {}
+      for (let key in this.userSelected) {
+        if (this.userSelected[key] !== this.listUsers[this.userSelected.uid][key]) {
+          finalObj[key] = this.userSelected[key]
+        }
+      }
+
+      if (Object.keys(finalObj).length === 0) {
+        this.hideChangeUser()
+        return
+      }
+
+      axios.patch(`${window.core_url}users/${this.userSelected.uid}/`, finalObj)
+        .then((response) => {
+          this.hideChangeUser()
+          delete finalObj['password']
+          Object.assign(this.listUsers[this.userSelected.uid], finalObj)
+        })
+        .catch((error) => {
+          console.log(error)
+          this.msgError = 'Erreur de la requète.'
+        })
     }
   },
   mounted: function () {
     axios.get(window.core_url + 'users/', {})
     .then((response) => {
-      console.log(response)
-
       response.data.forEach(function (element) {
         this.listUsers[element.uid] = element
         this.listUsers[element.uid].cotisant = 'non'
